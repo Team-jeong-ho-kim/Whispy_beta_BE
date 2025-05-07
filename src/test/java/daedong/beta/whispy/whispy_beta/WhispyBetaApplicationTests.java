cd16e09dfeae858a23700b7f0758cbbbfd8d482f
박주원 <qkrwndnjs7308@gmail.com>
2025-05-07 23:32:32 +0900

    feat : (#1) 프로젝트 세팅

---
+++ src/test/java/daedong/beta/whispy/whispy_beta/WhispyBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package daedong.beta.whispy.whispy_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhispyBetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
